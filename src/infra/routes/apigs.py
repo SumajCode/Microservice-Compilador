5fccde1bc8e3f412926ef949e9b012ad4c47af91
raulpsl <raulparedesld@gmail.com>
2025-06-03 18:20:57 -0400

    update: blueprint for parent and childs routes

--- src/infra/routes/apigs.py
+++ src/infra/routes/apigs.py
@@ -1,44 +1,22 @@
-from flask import Flask
-from flask import jsonify
-from flask import request
+from flask import Flask, Blueprint, jsonify
 from flask_cors import CORS
-from infra.controllers.CodeController import CodeController
+
+from infra.routes.CodeRoutes import blueprint as blueCode
 
 def createApp():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object('config.conf.BaseConf')
+    padreBlueprint = Blueprint('apicompilador', __name__, url_prefix='/apicompilador/v1')
+
+    @padreBlueprint.route('/')
+    def home():
+        return jsonify({
+            'data': f"{app.config['APP_NAME']+ '-' + app.config['APP_VERSION']} is running", 
+            'message' : 'OK', 
+            'status' : 200
+        })
+    
+    padreBlueprint.register_blueprint(blueCode)
+    app.register_blueprint(padreBlueprint)
     return app
-
-app = createApp()
-
-# @app.errorhandler(HTTPException)
-# def handle_error(e):
-#     return jsonify({
-#         'data': e.description, 
-#         'message' : e.name, 
-#         'status' : e.code
-#     }), e.code
-
-@app.route('/')
-def home():
-    return jsonify({
-        'data': f"{app.config['APP_NAME']+ '-' + app.config['APP_VERSION']} is running", 
-        'message' : 'OK', 
-        'status' : 200
-    })
-
-@app.route('/compilarCodigo', methods=['POST'])
-def compilarCodigo():    
-    return CodeController().compilar(request)
-
-@app.route('/evaluarCodigo')
-def compiler():
-    return 
-# @app.route('/error')
-# def handleErrors():
-#     if () :
-#         raise ServerError()
-
-if __name__ == '__main__' :
-    app.run()
