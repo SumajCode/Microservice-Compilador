d901a3ed6d3cf8ac9c498f6617859889918cf4b1
raulpsl <raulparedesld@gmail.com>
2025-06-03 18:30:47 -0400

    update: blueprint for parent and childs routes

---
+++ src/infra/routes/CodeRoutes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint
+from flask import request
+
+from infra.controllers.CodeController import CodeController
+
+compilador = CodeController()
+blueprint = Blueprint('code', __name__, url_prefix='/code')
+
+@blueprint.route('/compilar', methods=['GET'])
+def compilarCodigo():
+    return compilador.compilar(request)
+
+@blueprint.route('/evaluar', methods=['GET'])
+def evaluarCodigo():
+    return compilador.evaluar(request)
