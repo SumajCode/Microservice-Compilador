231233c4bb9aff9a1ed3bb96ebb568923a5bf97b
raulpsl <raulparedesld@gmail.com>
2025-07-19 20:57:56 -0400

    update: add flask restx for documentation

--- src/infra/routes/CodeRoutes.py
+++ src/infra/routes/CodeRoutes.py
@@ -1,17 +1,62 @@
-from flask import Blueprint
-from flask import request
+from flask import Blueprint, request
+from flask_restx import Namespace, Resource, fields
 
 from infra.controllers.CodeController import CodeController
 from infra.controllers.AssetController import AssetController
 
+api = Namespace('code', 'Code')
+
 compilador = CodeController()
 asset = AssetController()
-blueprint = Blueprint('code', __name__, url_prefix='/code')
 
-@blueprint.route('/compilar', methods=['POST'])
-def compilarCodigo():
-    return compilador.compilar(request)
+@api.route('/compilar', methods=['POST'])
+class CodeCompile(Resource):
+    def compilarCodigo(self):
+        """
+        Compila el c digo seg n el lenguaje y la entrada de datos.
 
-@blueprint.route('/evaluar', methods=['POST'])
-def evaluarCodigo():
-    return asset.evaluar(request)
+        Recibe una solicitud HTTP con los datos necesarios para la compilaci n,
+        incluyendo el c digo, el lenguaje de programaci n y las entradas y salidas
+        esperadas. El m todo configura el lenguaje y las entradas/salidas en el
+        objeto compilador y luego llama al m todo `compilar` de compilador para
+        realizar la compilaci n del c digo. Si el resultado no contiene errores,
+        devuelve una respuesta indicando xito; de lo contrario, devuelve un
+        mensaje de error.
+
+        Returns
+        -------
+        Response
+            Un objeto de respuesta que contiene el resultado de la compilaci n
+            del c digo, un mensaje de xito o error, el estado y el c digo de
+            respuesta.
+        """
+        return compilador.compilar(request)
+
+@api.route('/evaluar', methods=['POST'])
+class CodeAsset(Resource):
+    def evaluarCodigo(self):
+        """
+        Evalua el c digo seg n las entradas y salidas proporcionadas.
+
+        Recibe una solicitud HTTP con los datos necesarios para la evaluaci n,
+        incluyendo el c digo, el lenguaje de programaci n y las entradas y salidas
+        esperadas. El m todo configura el lenguaje y las entradas/salidas en el
+        objeto sandbox y luego llama al m todo `evaluar` de sandbox para
+        realizar la evaluaci n del c digo. Si el resultado no contiene errores,
+        devuelve una respuesta indicando xito; de lo contrario, devuelve un
+        mensaje de error.
+
+        Parameters
+        ----------
+        request : Request
+            La solicitud HTTP que contiene los datos necesarios para la evaluaci n,
+            incluyendo el c digo, el lenguaje de programaci n y las entradas y salidas
+            esperadas.
+
+        Returns
+        -------
+        Response
+            Un objeto de respuesta que contiene el resultado de la evaluaci n del
+            c digo, un mensaje de xito o error, el estado y el c digo de respuesta.
+        """
+        return asset.evaluar(request)
--- src/infra/routes/apigs.py
+++ src/infra/routes/apigs.py
@@ -1,22 +1,25 @@
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
-
-from infra.routes.CodeRoutes import blueprint as blueCode
+from infra.routes.CodeRoutes import api as blueCode
+from flask_restx import Api
 
 def createApp():
     app = Flask(__name__)
     CORS(app)
     app.config.from_object('config.conf.BaseConf')
-    padreBlueprint = Blueprint('apicompilador', __name__, url_prefix='/apicompilador/v1')
-
-    @padreBlueprint.route('/')
+    @app.route('/')
     def home():
         return jsonify({
             'data': f"{app.config['APP_NAME']+ '-' + app.config['APP_VERSION']} is running", 
             'message' : 'OK', 
             'status' : 200
         })
-    
-    padreBlueprint.register_blueprint(blueCode)
-    app.register_blueprint(padreBlueprint)
+
+    api = Api(
+        app,
+        title=app.config['APP_NAME'],
+        version=app.config['APP_VERSION'],
+        description='Descripcion',
+        doc='/docs')
+    api.add_namespace(blueCode)
     return app
